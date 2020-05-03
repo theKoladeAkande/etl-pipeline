31d5479f4be140bae2942a2476719b32920281d5
theKoladeAkande <akandekolade@gmail.com>
2020-05-03 19:43:23 +0100

    refactor: Modularize script
    modularize script to ensure functionality only involves
    creation and dropping of tables.

--- create_table.py
+++ create_table.py
@@ -1,6 +1,7 @@
-""" Creates tables """
-import psycopg2
 import contextlib
+
+import psycopg2
+from connection_manger import dbconnection_manager
 import sql_queries
 
 def psycopg2_error_handler(fn):
@@ -34,33 +35,6 @@ def data_definition(query: str, cursor, execute_many=False, data=None):
     elif "CREATE" in query:
         print("Table created successfuly")
 
-@contextlib.contextmanager
-def dbconnection_manager(dbname):
-    """
-    Context manager to handle database connection
-    param dbname: name of database
-    """
-    connection_state = False
-    try:
-        conn =  psycopg2.connect(f"host = localhost, user=postgres dbname={dbname}")
-        connection_state = True
-
-        print(f'connected to {dbname}...')
-        cur = conn.cursor()
-        conn.set_session(autocommit=True)
-        print('session set to autocommit')
-        yield cur
-
-    except psycopg2.Error as e:
-        raise
-
-    finally:
-        if connection_state:
-            print('closing cursors and connections')
-            cur.close()
-            conn.close()
-        else:
-            print('no connection made to the database')
 
 @psycopg2_error_handler
 def create_db(cursor, dbname):
@@ -74,13 +48,13 @@ def create_db(cursor, dbname):
 
 
 if __name__ == "__main__":
-    with dbconnection_manager(dbname='practice') as cursor:
+    with dbconnection_manager(dbname='practice') as (cursor,conn):
         create_db(cursor, "sparkifydb")
 
-    with dbconnection_manager(dbname="sparkifydb") as cursor:
+    with dbconnection_manager(dbname="sparkifydb") as (cursor, conn):
         for query in sql_queries.DROP_QUERIES:
                 data_definition(query, cursor)
 
-    with dbconnection_manager(dbname="sparkifydb") as cursor:
+    with dbconnection_manager(dbname="sparkifydb") as (cursor, conn):
         for query in sql_queries.CREATE_QUERIES:
             data_definition(query, cursor)
